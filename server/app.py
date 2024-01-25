98e815c017e012afb8720296bd129ad26ea53303
GMX\070038649 <segilber@ca.ibm.com>
2024-01-25 09:03:15 -0500

    .\server\app.py: demo.launch(): Removes certification files

--- server/app.py
+++ server/app.py
@@ -11,6 +11,5 @@ demo = gr.Interface(video_identity,
                     )
 
 if __name__ == '__main__':
-    demo.launch(server_name="0.0.0.0", server_port=7860, ssl_certfile="./cert/cert.pem",
-                ssl_keyfile="./cert/key.pem", ssl_verify=False)
+    demo.launch(server_name="0.0.0.0", server_port=7860, ssl_verify=False)
     # Cf. https://github.com/gradio-app/gradio/issues/2551
